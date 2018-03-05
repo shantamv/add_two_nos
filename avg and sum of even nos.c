eecdbd2ac6818d627cb61f5d129af14bf80b2d55
foxpixel <mohit98.2011@gmail.com>
2018-03-05 22:06:20 +0530

    Initial Commit

---
+++ avg and sum of even nos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum=0;
+	float avg; 
+	printf("enter a no to find the sum of even nos and average");
+	scanf("%d",&n);
+	for (i=0;i<=n;i++)
+	if(n%2==0)
+	{
+		sum+=i;
+	}
+		avg=sum/n;
+	printf("sum is =%d",sum);
+	printf("avg is =%f",avg);
+	return 0;	
+}
